72a68d544cbffdaa57eb585eebc3f938f1d3d28b
bvaught <wvaught3@gatech.edu>
2021-11-30 02:02:20 -0500

    Condense Files to Outsmart Safari

--- scripting.js
+++ scripting.js
@@ -191,48 +191,4 @@ if (prefersDarkScheme.matches){
           $( "#1conLink" ).removeClass( "active fw-bold fs-5 pb-0 pt-1");
         }
 });
-
-// $("#mode").click(function(){
-//   if($("#everything").hasClass("darkGeneric")){
-//     $("#timelineLine").removeClass("timeline__progressbarDark").addClass("timeline__progressbarLight");
-//     $("#mode").text("Dark Mode").removeClass("btn-light").addClass("btn-dark");
-//     $("#everything").toggleClass("darkGeneric");
-//     $("#everything").toggleClass("lightGeneric");
-//     $("#navbar").removeClass("darkModeNavbar").addClass("lightModeNavbar");
-//     $("#navbar2").removeClass("darkModeNavbar").addClass("lightModeNavbar");
-//     $("#footer").removeClass("darkModeNavbar").addClass("lightModeNavbar");
-//     $("#navbar").find(".nav-link").removeClass("text-light").addClass("text-dark");
-//     $("#navbar").find("svg").removeClass("vectorDark").addClass("vectorLight");
-//     $("#navbar2").find(".nav-link").removeClass("text-light").addClass("text-dark");
-//     $("#navbar2").find("svg").removeClass("vectorDark").addClass("vectorLight");
-//     $("#container").find( ".darkMode").removeClass("darkMode").addClass("lightMode");
-//     $("#container").find( ".darkGeneric").removeClass("darkGeneric").addClass("lightGeneric");
-//     $("#container").find( ".btn-outline-light").removeClass("btn-outline-light").addClass("btn-outline-dark");
-//     $("#contactForm").find(".darkModeForm").removeClass("darkModeForm")
-//   }else{
-//     $("#timelineLine").removeClass("timeline__progressbarLight").addClass("timeline__progressbarDark");
-//     $("#mode").text("Light Mode").removeClass("btn-dark").addClass("btn-light");
-//     $("#everything").toggleClass("darkGeneric");
-//     $("#everything").toggleClass("lightGeneric");
-//     $("#navbar").removeClass("lightModeNavbar").addClass("darkModeNavbar");
-//     $("#navbar2").removeClass("lightModeNavbar").addClass("darkModeNavbar");
-//     $("#footer").removeClass("lightModeNavbar").addClass("darkModeNavbar");
-//     $("#navbar").find(".nav-link").removeClass("text-dark").addClass("text-light");
-//     $("#navbar").find("svg").removeClass("vectorLight").addClass("vectorDark");
-//     $("#navbar2").find(".nav-link").removeClass("text-dark").addClass("text-light");
-//     $("#navbar2").find("svg").removeClass("vectorLight").addClass("vectorDark");
-//     $("#container").find( ".lightMode").removeClass("lightMode").addClass("darkMode");
-//     $("#container").find( ".lightGeneric").removeClass("lightGeneric").addClass("darkGeneric");
-//     $("#container").find( ".btn-outline-dark").removeClass("btn-outline-dark").addClass("btn-outline-light");
-//     $("#contactForm").find(".form-control").addClass("darkModeForm");
-//     }
-// });
-        // if(checkVisible($('#timelineEnd'), 'above')){
-        //   $("#timelineLine").fadeOut("slow");
-        //     $( "#timeline" ).stop().fadeTo( 500, 0.0);
-        // }else{
-        //   $("#timelineLine").fadeIn("slow");
-        //   $( "#timeline" ).stop().fadeTo( 300, 1);
-        // }
-
     });
